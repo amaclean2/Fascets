a00e7e5bb8513698919abc06afea55b49dc6122f
Andrew Maclean <andrew.n.maclean@gmail.com>
2024-06-11 14:34:10 -0700

    Initial Commit

---
+++ Sunday Peak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sunday Peak
+//
+//  Created by Andrew Maclean on 6/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sunday Peak/Sunday_PeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sunday_PeakApp.swift
+//  Sunday Peak
+//
+//  Created by Andrew Maclean on 6/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Sunday_PeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
